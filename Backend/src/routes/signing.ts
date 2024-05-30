13c789ec9ea7edbc6eaa9721d396e300c805fbc2
Aditya Karki <adityakarki201@gmail.com>
2024-05-30 21:13:26 +0530

    Done with signup and signin

--- Backend/src/routes/signing.ts
+++ Backend/src/routes/signing.ts
@@ -1,21 +1,103 @@
 import express,{Request,Response} from 'express';
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv'
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken'
 
+dotenv.config();
+const secretKey: string | undefined=process.env.SECRET_KEY;
 const signing=express.Router();
+const prisma=new PrismaClient()
 
+signing.post('/signup',async(req:Request,res:Response)=>{
 
-signing.post('/signup',(req:Request,res:Response)=>{
+    const {name,email,password}=req.body;
+    try {
+        
+        const newUser=await prisma.user.create({
+            data:{
+                name,
+                email,
+                password
+            }
+        })
+
+        if(!newUser){
+            res.status(404).json({
+                msg:'Incorrect Data!'
+            })
+            return;
+        }
+
+        const token=jwt.sign({
+            payload:newUser.email           
+        }, secretKey as string)
+
+        res.status(200).json({
+            msg: token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(411).json({
+            msg:"Something went wrong please try again!!"
+        })
+    }
     
-    res.status(200).json({
-        msg:"This is the correct route!!"
-    })
 })
 
 signing.post('/signin',(req:Request,res:Response)=>{
 
-    res.status(200).json({
-        msg:"This is the signin route!!"
-    })
+    const {email,password}=req.body;
+    const tokenWithBearer=req.headers.authorization;
+    const token=tokenWithBearer?.split(' ')[1];
+
+    
+    try {
+
+        const decode=jwt.verify(token as string,secretKey as string) as JwtPayload;
+        console.log(decode);
+        if(decode.payload!==email){
+            res.json({
+                msg:"User does not match, with the token"
+            })
+            return;
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            msg:"Invalid Token"
+        })
+    }
+
+    try {
+        const verifyUser=prisma.user.findFirst({
+            where:{
+                email,
+                password
+            }
+        })
+        console.log(verifyUser)
+        if(!verifyUser){
+            res.status(404).json({
+                msg:"User not found!! Please try signing up"
+            })
+        }
+        res.status(200).json({
+            msg:"User signed In successfully"
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            msg:'Something went wrong! Please try again'
+        })
+    }
+
+
+    
 
 })
 
 export default signing;
+
