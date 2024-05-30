e11bd76c4e73f7716f0b59cbc79b50403034784f
Aditya Karki <adityakarki201@gmail.com>
2024-05-30 18:16:16 +0530

    Defined Prisma for database

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import signing from "./routes/signing";
 import blogging from "./routes/blogging";
+import cors from 'cors'
 const app=express();
 const port=3000;
 
+app.use(cors())
+
+app.use(express.json())
 
 app.use('/api/v1/user',signing);
 
